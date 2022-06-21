ebb1cf1f80315d84216df86afc9197da7349a877
Pablo San Martin <psanma98@gmail.com>
2022-06-20 21:25:54 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/navbar.js';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hola Mundo</h1>
+    </div>
+  );
+}
+
+export default App
